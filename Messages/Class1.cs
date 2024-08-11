260e92cf361557551e2f40df4c2fc35b41c7dfd4
Sladamos <sladamo@wp.pl>
2024-08-11 16:02:19 +0200

    MS-2 two simple messages created

---
+++ Messages/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace Messages
+{
+    public interface IPatientRegisteredMessage
+    {
+        Guid patientId { get; }
+        String patientFirstName { get; }
+        String patientSecondName { get; }
+        String patientPesel {  get; }
+        DateTime patientDateOfBirth { get; }
+        String patientCity { get; }
+        String patientPostalCode { get; }
+        String patientStreet { get; }
+        String patientPhoneNumber { get; }
+        int patientHouseNumber { get; }
+        int patientApartmentNumber { get; }
+    }
+
+    public interface IPatientUnregisteredMessage
+    {
+        Guid pateintId { get; }
+    }
+}
