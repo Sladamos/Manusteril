46b1c232d74b3bebcb3dc1c4aca4b5519b5aaa55
Sladamos <sladamo@wp.pl>
2024-09-08 21:24:47 +0200

    MS-36 working emergency

--- Emergency/Handler/PatientVisitRegistrationDeclinedHandler.cs
+++ Emergency/Handler/PatientVisitRegistrationDeclinedHandler.cs
@@ -11,30 +11,16 @@ using System.Threading.Tasks;
 
 namespace Emergency.Handler
 {
-    internal class PatientVisitRegistrationInfoHandler : IBusConsumer<IPatientVisitRegistrationAcceptedResponse>, IBusConsumer<IPatientVisitRegistrationDeclinedResponse>
+    internal class PatientVisitRegistrationDeclinedHandler : IBusConsumer<IPatientVisitRegistrationDeclinedResponse>
     {
         private ILog logger;
 
-        public PatientVisitRegistrationInfoHandler(ILog logger)
+        public PatientVisitRegistrationDeclinedHandler(ILog logger)
         {
             this.logger = logger;
         }
 
-        public string QueueName => "emergency_visitInfo";
-
-        public async Task Consume(ConsumeContext<IPatientVisitRegistrationAcceptedResponse> context)
-        {
-            try
-            {
-                var message = context.Message;
-                logger.Info($"Otrzymano akceptację przyjęcia: {message}");
-                Console.WriteLine($"Oddział {message.WardIdentifier} informuje, że może przyjąć pacjenta {message.PatientPesel}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Błąd przy obsłusze akceptacji przyjęcia: {ex.Message}");
-            }
-        }
+        public string QueueName => "emergency_visitDeclined";
 
         public async Task Consume(ConsumeContext<IPatientVisitRegistrationDeclinedResponse> context)
         {
